cdd5f8d5005205a69af3c77956559a0a794c04fa
klav28 <klav28@gmail.com>
2023-09-17 22:57:48 +0300

    pre-release

--- src/components/Vars.js
+++ src/components/Vars.js
@@ -1,9 +1,2 @@
 export const BASE_URL = 'https://6500b76118c34dee0cd5552f.mockapi.io/';
 export const LSKEY = 'rent-favorites';
-
-export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
-export const noPosterUrl = new URL(
-  '../images/no-car-photo.jpg',
-  import.meta.url
-);
-export const noAvatar = new URL('../images/no-avatar.png', import.meta.url);
